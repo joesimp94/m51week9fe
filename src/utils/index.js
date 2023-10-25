edbd9b7c8ff90468b732fad3ee41b5bcc91cdde5
Joe Simpson <josephsimpson94@googlemail.com>
2023-10-25 10:54:31 +0100

    enhancement: added login logic

--- src/utils/index.js
+++ src/utils/index.js
@@ -17,3 +17,22 @@ export const registerUser = async (username, email, password) => {
     console.log(error);
   }
 };
+
+export const loginUser = async (username, password) => {
+  try {
+    const response = await fetch(`http://localhost:5001/user/login`, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    });
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
